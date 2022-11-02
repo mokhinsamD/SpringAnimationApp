f70418574aa6a19a2a37bfd6c070a9acd1c6b757
Darina Samokhina <mokhinsam@yandex.ru>
2022-11-02 14:59:40 +0300

    Initial Commit

---
+++ SpringAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationApp
+//
+//  Created by Дарина Самохина on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
