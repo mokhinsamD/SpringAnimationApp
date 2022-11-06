ab7a32a6f9bc018014839ced13e6479216372347
Darina Samokhina <mokhinsam@yandex.ru>
2022-11-06 10:32:38 +0300

    final vers

--- SpringAnimationApp/Models/Animation.swift
+++ SpringAnimationApp/Models/Animation.swift
@@ -13,12 +13,20 @@ struct Animation {
     let duration: Double
     let delay: Double
     
+    var description: String {
+        """
+        preset: \(preset)
+        curve: \(curve)
+        force: \(String(format: "%.2f", force))
+        duration: \(String(format: "%.2f", duration))
+        delay: \(String(format: "%.2f", delay))
+        """
+    }
+    
     static func getAnimation() -> Animation {
-        let dataStore = DataStore()
-        
-        return Animation(
-            preset: dataStore.presets.randomElement() ?? "shake" ,
-            curve: dataStore.curves.randomElement() ?? "easeIn",
+        Animation(
+            preset: (DataStore.shared.presets.randomElement()?.rawValue  ?? "shake"),
+            curve: DataStore.shared.curves.randomElement()?.rawValue  ?? "easeIn",
             force: Double.random(in: 1...2),
             duration: Double.random(in: 0.8...2),
             delay: Double.random(in: 0.2...0.5)
--- SpringAnimationApp/ViewControllers/ViewController.swift
+++ SpringAnimationApp/ViewControllers/ViewController.swift
@@ -8,51 +8,31 @@
 import UIKit
 import SpringAnimation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var animationView: SpringView!
-    
-    @IBOutlet var presetLabel: UILabel!
-    @IBOutlet var curveLabel: UILabel!
-    @IBOutlet var forceLabel: UILabel!
-    @IBOutlet var durationLabel: UILabel!
-    @IBOutlet var delayLabel: UILabel!
+    @IBOutlet var animationLabel: UILabel! {
+        didSet {
+            animationLabel.text = animation.description
+        }
+    }
     
     private var animation = Animation.getAnimation()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        animationView.layer.cornerRadius = 10
-        updateLabelsValue()
-    }
 
     @IBAction func runButtonDidPressed(_ sender: UIButton) {
-        
-        if sender.currentTitle == "Run" {
-            sender.setTitle("Run \(animation.preset)", for: .normal)
-        }
+        animationLabel.text = animation.description
         
         animationView.animation = animation.preset
         animationView.curve = animation.curve
         animationView.force = animation.force
-        animationView.duration = animation.delay
+        animationView.duration = animation.duration
         animationView.delay = animation.delay
         animationView.animate()
         
-        updateLabelsValue()
         animation = Animation.getAnimation()
         
         sender.setTitle("Run \(animation.preset)", for: .normal)
     }
 }
 
-extension ViewController {
-    func updateLabelsValue() {
-        presetLabel.text = animation.preset
-        curveLabel.text = animation.curve
-        forceLabel.text = String(format: "%.2f", animation.force)
-        durationLabel.text = String(format: "%.2f", animation.duration)
-        delayLabel.text = String(format: "%.2f", animation.delay)
-    }
-}
 
